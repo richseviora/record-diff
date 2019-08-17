a48c7fd5b6db981a82b9407f06c1092e8cbf8886
Rich Seviora <richard.seviora@gmail.com>
2019-08-16 20:51:39 -0700

    Refactor specs

--- spec/zhuli/matcher_spec.rb
+++ spec/zhuli/matcher_spec.rb
@@ -8,23 +8,33 @@ RSpec.describe ZhuLi::Matcher do
     OpenStruct.new(hsh)
   end
   describe '#process' do
-    it 'assigns the values' do
-      before_ary = [create_obj(id: 1, a: 1),
-                    create_obj(id: 2, a: 2),
-                    create_obj(id: 4, a: 1)]
-      after_ary = [create_obj(id: 2, a: 1),
-                   create_obj(id: 3, a: 2),
-                   create_obj(id: 4, a: 1)]
-      matcher = described_class.new(before: before_ary, after: after_ary)
-                               .process
-      expect(matcher.results.any? { |r| r.added? == true && r.id == 3 })
-        .to eql(true)
-      expect(matcher.results.any? { |r| r.dropped? == true && r.id == 1 })
-        .to eql(true)
-      expect(matcher.results.any? { |r| r.unchanged? == true && r.id == 4 })
-        .to eql(true)
-      expect(matcher.results.any? { |r| r.changed? == true && r.id == 2 })
-        .to eql(true)
+    subject(:matcher) do
+      described_class.new(before: before_ary, after: after_ary)
+                     .process
+    end
+
+    let(:before_ary) do
+      [create_obj(id: 1, a: 1),
+       create_obj(id: 2, a: 2),
+       create_obj(id: 4, a: 1)]
+    end
+    let(:after_ary) do
+      [create_obj(id: 2, a: 1),
+       create_obj(id: 3, a: 2),
+       create_obj(id: 4, a: 1)]
+    end
+
+    it 'picks up one added result' do
+      expect(matcher.results.one? { |r| r.added? == true }).to be(true)
+    end
+    it 'picks up one dropped result' do
+      expect(matcher.results.one? { |r| r.dropped? == true }).to be(true)
+    end
+    it 'picks up one unchanged result' do
+      expect(matcher.results.one? { |r| r.unchanged? == true }).to be(true)
+    end
+    it 'picks up one changed result' do
+      expect(matcher.results.one? { |r| r.changed? == true }).to be(true)
     end
   end
 end
--- zhuli.gemspec
+++ zhuli.gemspec
@@ -35,4 +35,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'rubocop', '~> 0.74'
+  spec.add_development_dependency 'rubocop-rspec', '~> 1.35'
 end
