8055f87dba3e692a351a7811edf8d6e8865d7da9
Rich Seviora <richard.seviora@gmail.com>
2019-08-18 11:33:39 -0700

    Adds the Result Set collection

--- lib/zhuli/matcher.rb
+++ lib/zhuli/matcher.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ZhuLi
+  # Handles matching results.
   class Matcher
     attr_reader :before, :after, :id_method, :before_grouped, :after_grouped
     attr_reader :results
